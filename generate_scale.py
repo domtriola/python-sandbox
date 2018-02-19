6f8cbdfc29946119fd7454dd13a77b21aede8471
Dominick Triola <dominick@ignovadesign.com>
2018-02-19 12:06:40 -0800

    Create scale generator

---
+++ generate_scale.py
@@ -0,0 +1,42 @@
+"""
+A script to generate scales.
+
+Ex: `python generate_scale.py D minor`
+"""
+
+import sys
+
+def step_to_int(step):
+    return {'h': 1, 'W': 2}[step]
+
+class ScaleGenerator:
+    """
+    Can generate each note found in a particular scale. Assumes all accidentals
+    are sharp.
+    """
+
+    NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+    SCALE_SEQUENCES = {
+        'major': ['W', 'W', 'h', 'W', 'W', 'W', 'h'],
+        'minor': ['W', 'h', 'W', 'W', 'h', 'W', 'W']
+    }
+
+    @classmethod
+    def scale(cls, root, key):
+        sequence = cls.SCALE_SEQUENCES[key]
+        root_idx = cls.NOTES.index(root)
+
+        result = [root]
+        step_total = 0
+        for step in sequence:
+            step_total += step_to_int(step)
+            result.append(cls.NOTES[(root_idx + step_total) % len(cls.NOTES)])
+
+        return result
+
+
+if __name__ == "__main__":
+    ROOT = sys.argv[1]
+    KEY = sys.argv[2]
+
+    print(ScaleGenerator.scale(ROOT, KEY))
